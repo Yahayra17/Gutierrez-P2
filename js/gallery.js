8fb18278fc84c7c09a08ee5340ab9601d2409a83
Yahayra17 <50460509+Yahayra17@users.noreply.github.com>
2021-05-10 11:42:17 -0400

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -31,32 +31,42 @@ function animate() {
 }
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
+function rotate(){
+  if($(".moreIndicator").hasClass("rot90")){
+    $(".moreIndicator").removeClass("rot90").addClass("rot270");
+    $(".details").slideToggle("slow");
+  } else{
+    $(".moreIndicator").removeClass("rot270").addClass("rot90");
+    $(".details").slideToggle("slow");
+  }
+}
+
 
 function swapPhoto() {
 
-  if(mCurrentIndex >= mImages.length)
+  if (mCurrentIndex >= mimage.lenth)
   {
     mCurrentIndex = 0;
   }
 
-  if(mCurrentIndex < 0) {
-    mCurrentIndex = mImages.length-1;
-  }
+   if (mCurrentIndex < 0 ){
+     mCurrentIndex = mImages.length-1;
+   }
 
-  document.getElementById('photo').src = mImages[mCurrentIndex].img;
+    document.getElementById('photo').src = mImages [mCurrentIndex].img;
 
-  var location = document.getElementsByClassName('location')[0];
-  location.innerHTML = "Location: " + mImages[mCurrentIndex].location;
+    var location = document.getElementsByClassName('location')[0];
+    location.innerHTML = "Location: " + mImages[mCurrentIndex].location;
 
-  var description = document.getElementsByClassName('description')[0];
-  description.innerHTML = "Description: " + mImages[mCurrentIndex].description;
+    var decription = document.getElementsByClassName9('description')[0];
+    description.innerHTML = "Description: "  + mImages[mCurrentIndex].description;
 
-  var date = document.getElementsByClassName('date')[0];
-  date.innerHTML = "Date: " + mImages[mCurrentIndex].date;
+    var date = document.getElementsByClassName('date')[0];
+    date.innerHTML = "Date: " + mImages[mCurrentIndex].date;
 
-  mLastFrameTime = 0;
-  mCurrentIndex += 1;
-	console.log('swap photo');
+	   mLastFrameTime = 0;
+     mCurrentIndex += 1;
+      console.log('swap photo');
 }
 
 // Counter for the mImages array
@@ -73,14 +83,14 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'images.json';
+var mUrl = '../images.json';
 
-function fetchJSON() {
-  mRequest.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-         mJson = JSON.parse(mRequest.responseText);
-         iterateJSON();
-      }
+function fetchJSON(){
+mRequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       mJson =  JSON.parse(mRequest.responseText);
+       iterateJSON();
+    }
   };
   mRequest.open("GET", mUrl, true);
   mRequest.send();
@@ -99,7 +109,7 @@ function makeGalleryImageOnloadCallback(galleryImage) {
 $(document).ready( function() {
   fetchJSON();
 	// This initially hides the photos' metadata information
-	// $('.details').eq(0).hide();
+ //	$('.details').eq(0).hide();
 
 });
 
@@ -109,11 +119,11 @@ window.addEventListener('load', function() {
 
 }, false);
 
-function iterateJSON() {
-  for(x = 0; x < mJson.images.length; x++)
+function  iterateJSON(){
+  for(x = 0; x < mJson; x ++)
   {
     mImages[x] = new GalleryImage();
-    mImages[x].location = mJson.images[x].imgLocation;
+    mImages[x].location = mJson.images[x].imageLocation;
     mImages[x].description = mJson.images[x].description;
     mImages[x].date = mJson.images[x].date;
     mImages[x].img = mJson.images[x].imgPath;
@@ -121,8 +131,14 @@ function iterateJSON() {
 }
 
 function GalleryImage() {
-	var location;
-	var description;
-	var date;
+  var location;
+  var description;
+  var date;
   var img;
 }
+
+$("#nextPhoto").position({
+    my: "right bottom",
+    at: "right bottom",
+    of: "#nav"
+});
